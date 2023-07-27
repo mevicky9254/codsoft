dea5b195d3aa91a611c831451bdb98b70e8b0fcf
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-07-27 20:38:44 +0530

    Created the API for the WordCount

---
+++ WordCounter/WorCounterApp/src/main/java/com/codsoft/WorCounterAppApplication.java
@@ -0,0 +1,13 @@
+package com.codsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorCounterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorCounterAppApplication.class, args);
+	}
+
+}
---
+++ WordCounter/WorCounterApp/src/main/java/com/codsoft/controller/WordCounterController.java
@@ -0,0 +1,18 @@
+package com.codsoft.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WordCounterController {
+	
+	@PostMapping("/countWords")
+    public int countWords(@RequestBody String text) {
+        String[] words = text.trim().split("\\s+");
+        return words.length;
+    }
+	
+}
