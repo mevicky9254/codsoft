c2d832f8f27956645133543c097eb3674638273a
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-08-02 12:53:05 +0530

    created the apis for generating, validating and getting the random number

---
+++ NumberGame/src/main/java/com/codsoft/controller/NumberController.java
@@ -0,0 +1,45 @@
+package com.codsoft.controller;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="*")
+public class NumberController {
+	int result=0;
+	
+	@GetMapping("/getRandomNumber")
+	public int getRandomNumber() {
+		
+		//System.out.println("result "+result);
+		
+		return result;
+		
+	}
+	
+	
+	
+	@PostMapping("/matchNumber")
+	public boolean matchRandomNumber(@RequestBody Integer number) {
+		
+		 Random random = new Random();
+		 
+	     int randomNumber = random.nextInt(100) + 1;
+		 result=randomNumber;
+		 
+     //	 System.out.println(randomNumber);
+	     
+	     if(number==randomNumber) {
+	    	 return true;
+	     }else {
+	    	return  false;
+	     }
+	}
+	
+
+}
